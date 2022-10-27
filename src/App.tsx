c4569b6c1c3cff1cb6afab69a927a2865a81425a
Julie Jacobson <juliejacobson@Julies-MacBook-Pro.local>
2022-10-26 17:15:37 -0700

    finished up code to display sign in and sign up:

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+// import './App.css'
+import { Container } from 'react-bootstrap'
+import { SignUp } from './pages/SignUp'
+import { Route, Routes } from 'react-router-dom'
+import { SignIn } from './pages/SignIn'
+
 
 function App() {
 
   return (
-    <>
-    test 
-    </>
+    <div className='background d-flex align-items-center justify-content-center'>
+      <Routes>
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-in" element={<SignIn/>} />
+
+      </Routes>
+    </div>
 
   )
 }
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,18 +1,21 @@
+
 import { Button, Container, Form, InputGroup, Stack } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
-export function SignUp() {
+
+export function SignIn() {
     return (
         <Container className='content-container' style={{width: '35%', minWidth: '250pt'}}>
             <Stack gap={3}>
             <h1 style={{color: 'var(--primary)'}} className='align-self-center'>REPS</h1>
 
+
             <div className='d-sm-flex flex-column'>
-                <h2>Sign up</h2>
+                <h2>Sign in</h2>
                 <div style={{gap: '.5em'}} className='d-flex'>
-                    <p>Already have an account?</p>
-                    <NavLink to='/sign-in' className='text-btn' >
-                        Sign in
+                    <p>Don't have an account?</p>
+                    <NavLink to='/sign-up' className='text-btn' >
+                        Sign up
                     </NavLink>
                 </div>
                 
@@ -24,34 +27,24 @@ export function SignUp() {
                     <Form.Control type="text"/>
                 </Form.Group>
 
-                <Form.Group>
-                    <Form.Label className='form-label'>Email</Form.Label>
-                    <Form.Control type="email"/>
-                </Form.Group>
 
                 <Form.Group>
                     <Form.Label className='form-label'>Password</Form.Label>
                     <Form.Control type="password"/>
                 </Form.Group>
-
-                <Form.Group>
-                    <Form.Label className='form-label'>Retype Password</Form.Label>
-                    <Form.Control type="password"/>
-                </Form.Group>
             </Form>
 
             <div className='d-flex align-self-center flex-column align-items-center' style={{width: '160pt', paddingTop: '2rem'}} >
-                <Button variant="success" className='custom-btn'>SIGN UP</Button>
+                <Button variant="success" className='custom-btn'>SIGN IN</Button>
                 <h4>OR</h4>
                 <Button style={{gap:'.5em'}} variant="light" className='custom-btn secondary d-flex align-items-center justify-content-center'>
                     <img src='/imgs/google-icon.png' height='20px'/>
-                    Sign up using Google
+                    Sign in using Google
                     </Button>
 
             </div>
+
             </Stack>
-
-
         </Container>
     )
 }
